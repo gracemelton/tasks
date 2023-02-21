f72ae2cc2611806d7c70ace3f20f3c0d06fd76b5
Grace Melton <gmelton@udel.edu>
2023-02-21 14:52:37 -0500

    solved 3, 4,and 5

--- src/functions.ts
+++ src/functions.ts
@@ -12,6 +12,7 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
+    // eslint-disable-next-line @typescript-eslint/no-inferrable-types
     let sum: number = 0;
     if (first > 0) {
         sum += first;
@@ -30,7 +31,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -38,7 +39,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message[message.length - 1] === "?" ? true : false;
 }
 
 /**
@@ -47,5 +48,13 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    }
+
+    if (word === "no" || word === "NO") {
+        return false;
+    }
+
+    return null;
 }
